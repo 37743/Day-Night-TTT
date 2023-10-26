c11c58b9f693def43254c53fbae7c2b4d404ec97
37743 <yousef.ibrahim.gomaa@gmail.com>
2023-10-26 22:13:52 +0300

    Uploaded DFS, BFS, and UCS

---
+++ scripts/datastructures/queue.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, value, move=0):
+        self.value = value
+        self.move = move
+        self.next = None
+ 
+ 
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.size = 0
+    
+    def __str__(self):
+        current = self.front
+        string = ""
+        while current:
+            string += str(current.value) + " "
+            current = current.next
+        return string[:-1]
+  
+    def enqueue(self, value, move=0):
+        new_node = Node(value, move)
+        if self.front is None:
+            self.front = new_node
+        else:
+            current = self.front
+            while current.next != None:
+                current = current.next
+            current.next = new_node
+        self.size += 1
+ 
+    def is_empty(self):
+        return self.size == 0
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Queue is empty.")
+        remove = self.front
+        self.front = self.front.next
+        self.size -= 1
+        return remove.move
+ 
+    def top(self):
+        if self.is_empty():
+            raise Exception("Queue is empty.")
+        return self.front.value
+ 
+    def get_size(self):
+        return self.size
